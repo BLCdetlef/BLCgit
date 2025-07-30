459241b463c7238a083979d16f89d7c36a4966c0
BLCdetlef <detlef.hau@gmail.com>
2025-07-30 06:11:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let num = '';
+function press(d) { if (num.length < 3) num += d; update(); }
+function erase() { num = num.slice(0, -1); update(); }
+function update() { document.getElementById('display').textContent = num.padEnd(3, '-'); }
+function play(src) {
+  new Audio(src).play().catch(console.log);
+}
+function playNumber() {
+  const file = `sounds/${num||'000'}.mp3`;
+  fetch(file).then(r => {
+     if (r.ok) play(file);
+     else play('sounds/999.mp3');
+  }).catch(()=>play('sounds/999.mp3'));
+}
+function playIntro() { play('sounds/BLC_introaudio.mp3'); }
+update();
